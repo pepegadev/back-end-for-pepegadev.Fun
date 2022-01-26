0ad17437cd55cd3e4c84bed3dbadd2eab4c0dcfa
pepegadev <98327273+pepegadev@users.noreply.github.com>
2022-01-26 10:40:13 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+//our main file entry point .
+
 const express = require('express');
 const bodyparser=require('body-parser');
+//bodyparser : to parse request as json.
 require('dotenv').config('mysql')
+//mysql so we can use mysql
 var mysql = require('mysql');
 const app =express();
 app.use(express.json());
 app.use(bodyparser.json());
+//this is routes : tokens and sadge
 const route =require('./routes/tokens');
 const pop=require('./routes/sadge');
 app.use('/tokens',route);
 app.use('/users',pop);
+// to connect to mysql 
 var mysqlconnection = mysql.createConnection(
     {   
       host:'localhost',
       user:'pepecszq_popa',
-      password:'dali21477983@',
-      database:'pepecszq_token'   });
+      password:'',
+      database:''   });
      mysqlconnection.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
@@ -26,6 +32,6 @@ var mysqlconnection = mysql.createConnection(
 
 
 
-
-app.listen(5000,()=> console.log("ddddd"));
+// the port 
+app.listen(5000,()=> console.log("it's working"));
 
